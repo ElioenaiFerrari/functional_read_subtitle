44fbca448cdcb61fc0e1c3d5520d65dfbe8f830e
Elioenai Ferrari <elioenaiferrari@gmail.com>
2020-08-03 16:26:28 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const path = require('path')
 const {
   filterByExtension,
-  readDir
+  readDir,
+  readSeveralFiles,
 } = require('./utils');
 
 
 const pathDir = path.join(__dirname, '..', 'legendas');
 
 readDir(pathDir).then(
-  files => filterByExtension(files, '.srt'),
+  files => filterByExtension(files, '.srt')
+).then(
+  readSeveralFiles
 ).then(console.log);
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,11 +2,11 @@ const fs = require('fs')
 const path = require('path')
 
 
-function readDir(pathDir) {
+function readDir(dirPath) {
   return new Promise((resolve, reject) => {
     try {
-      const dir = fs.readdirSync(pathDir);
-      const readFile = file => path.join(pathDir, file);
+      const dir = fs.readdirSync(dirPath);
+      const readFile = file => path.join(dirPath, file);
       const files = dir.map(readFile);
 
       resolve(files);
@@ -23,7 +23,31 @@ function filterByExtension(array = [], extension = '') {
 }
 
 
+function readUniqueFile(filePath) {
+  return new Promise((resolve, reject) => {
+    try {
+      const content = fs.readFileSync(filePath, {
+        encoding: 'utf-8'
+      });
+
+      resolve(content.toString());
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+
+function readSeveralFiles(severalFilesPath = []) {
+  return Promise.all(
+    severalFilesPath.map(readUniqueFile)
+  );
+}
+
+
 module.exports = {
   readDir,
-  filterByExtension
+  filterByExtension,
+  readUniqueFile,
+  readSeveralFiles
 }
